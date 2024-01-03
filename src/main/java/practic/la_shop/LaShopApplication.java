9e0d3347cfe0e787c2bf6c5b445f390fe1405734
bahertylop <levichankin@gmail.com>
2024-01-03 12:25:08 +0300

    spring rep commit

---
+++ src/main/java/practic/la_shop/LaShopApplication.java
@@ -0,0 +1,13 @@
+package practic.la_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaShopApplication.class, args);
+	}
+
+}
